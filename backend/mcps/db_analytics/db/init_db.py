47535e022fde6682eb198c0a4c444cd1c9b6fa44
JavierB <javier.bastande@nuvolar.eu>
2025-11-25 13:55:15 +0100

    Update directory path

--- backend/mcps/db_analytics/db/init_db.py
+++ backend/mcps/db_analytics/db/init_db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
 import psycopg2
-from backend.models import Base
+from models import Base
 from urllib.parse import urlparse
 from dotenv import load_dotenv
 
