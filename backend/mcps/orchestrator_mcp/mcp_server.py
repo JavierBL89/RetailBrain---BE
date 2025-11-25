65646eeb5994ab9bbb7f703c252b9ea129c72e9f
JavierB <javier.bastande@nuvolar.eu>
2025-11-25 12:36:10 +0100

    Comment out some lines of code

--- backend/mcps/orchestrator_mcp/mcp_server.py
+++ backend/mcps/orchestrator_mcp/mcp_server.py
@@ -42,10 +42,14 @@ def route_request( action: str,
         #return upsert_product(product or {})  ## Product vector db insertion 
     
     elif action == "semantic_products_search":
-        return vector_search(conversation_id, user_query or {})
+        #return vector_search(conversation_id, user_query or {})
+        return "Route request tool reached for semantic_products_search"
+    
     elif action == "health":
         return health()
+    
     elif action == "echo":
+
         message = action.get("message", "")
         return echo(message)
     else:
@@ -75,46 +79,6 @@ def upsert_product(product: dict) -> str:
     }, indent=2)
 
 
-
-chat_memory={} # Dict[str, List[Dict[str, str]]]
-
-def vector_search(conversation_id:str, user_query: dict):
-    """
-    Perform a semantic search for products based on user query.
-    """
-    if conversation_id not in chat_memory:
-        chat_memory[conversation_id] = []
-
-
-    current_user_text = user_query.get('query', '')
-    # Add user query to conversation history
-    chat_memory[conversation_id].append({"role": "user", "content": current_user_text})
-
-    # Call LLaMA API to extract intent/entities
-    llm_response = query_gpt_api(chat_memory[conversation_id])
-
-    if llm_response is None:
-        llm_response = "Sorry, something went wrong."
- 
-
-    if isinstance(llm_response, dict) and llm_response.get("structured_data"):
-        # convert string to dict and build the embedded document
-        try:
-            structured_query = llm_response["structured_data"]
-            print("Structured Query Extracted:", structured_query)
-            print(process_search(structured_query))
-            #return process_search(structured_query)
-
-        except json.JSONDecodeError:
-            print("❌ Could not parse structured query:", structured_query)
-    
-    # Extract Assistant action if present
-    assistant_text = llm_response if isinstance(llm_response, str) else llm_response.get("user_text", str(llm_response))
-
-    # Save LLM response to conversation history
-    chat_memory[conversation_id].append({"role": "assistant", "content": assistant_text})
-    # Save last message to session for state persistence
-    return {"result": llm_response}
 
 
 # ------------------------------------------------------
